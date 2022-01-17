cd857d263321c3f93d6e69cccb57648845cf0655
Ashishvkale <97302990+Ashishvkale@users.noreply.github.com>
2022-01-17 09:54:13 +0530

    Header css
    Changing styling by using react external sheet

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+
+function App() {
+  
+  return (
+    <div>
+      <Header/>
+      <button> Add </button>
+      <div>
+        <span>Name</span><br />
+        <span>Phone</span>
+      </div>
+    </div>
+  );
+}
+export default App;
