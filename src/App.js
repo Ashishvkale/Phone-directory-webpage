89dc53d9c996a242da60315dc84665b0a86afaac
Ashishvkale <97302990+Ashishvkale@users.noreply.github.com>
2022-01-17 11:22:20 +0530

    Added Delete button

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="component-container">
         <Header />
           <div className="component-body-container">
             <button className="custom-btn add-btn">Add</button>
-
             <div className="grid-container heading-container">
               <span className="grid-item name-heading">Name</span>
-              <span className="grid-item phone-heading">Phone</span>
+              <span className="grid-item phone-heading">Phone</span>              
             </div>
+            {
+              subscribers.map(sub=> {
+                return <div key={sub.id} className="grid-container">
+                  <span className="grid-item">{sub.name}</span>
+                  <span className="grid-item">{sub.phone}</span>
+                  <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+                  </div>
+              })
+            }
           </div>
       </div>
     );
